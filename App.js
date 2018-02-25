d75f7c5093ffdf0a44777f2e3c59bb6508de4498
David Mrnustik <david.mrnustik@hotmail.com>
2018-02-25 18:50:45 +0100

    Add ImagePicker

--- App.js
+++ App.js
@@ -1,8 +1,58 @@
 import React from 'react';
 import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
-import { StyleSheet, Text, View, AppRegistry, ScrollView, Image, TouchableOpacity, Title, Animated } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  AppRegistry,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  Title,
+  Animated,
+  ImageEditor,
+} from 'react-native';
 import Header from './Header';
 import { TabNavigator, StackNavigator, DrawerNavigator } from 'react-navigation';
+import { ImagePicker } from 'expo';
+
+export default class App extends React.Component {
+  state = {
+    image: null,
+  }
+  pickImage = () => {
+    ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [2, 1]
+    }).then((result) => {
+      if (result.cancelled) {
+        return
+      }
+
+      ImageEditor.cropImage(result.uri, {
+        offset: { x: 0, y: 0},
+        size: { width: result.width, height: result.height },
+        displaySize: { width: 200, height: 100 },
+        resizeMode: 'contain',
+      },
+      (uri) => this.setState(() => ({ image: uri})),
+      () => console.log('Error'))
+    })
+  }
+  render() {
+    const { image } = this.state;
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity onPress={this.pickImage}>
+          <Text>Open Camera Roll</Text>
+        </TouchableOpacity>
+        {image && (
+          <Image style={styles.img} source={{uri: image}} />
+        )}
+      </View>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -11,39 +61,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   img: {
-    width: 200,
-    height: 200
+    width: 150,
+    height: 150,
+    resizeMode: 'contain',
+    backgroundColor: 'black',
   }
 })
-
-// decay - initial velocity and slow to a stop
-// spring - spring physics model
-// timing - animated a value over time
-
-export default class App extends React.Component {
-  state = {
-    opacity: new Animated.Value(0),
-    width: new Animated.Value(0),
-    height: new Animated.Value(0)
-  }
-  componentDidMount() {
-    const { opacity, width, height } = this.state;
-
-    Animated.timing(opacity, { toValue: 1, duration: 1000 })
-      .start()
-
-    Animated.spring(width, { toValue: 300, speed: 5}).start()
-    Animated.spring(height, { toValue: 300, speed: 5}).start()
-  }
-  render() {
-    const { opacity, width, height } = this.state;
-    return (
-      <View style={styles.container}>
-        <Animated.Image
-          style={[styles.img, { opacity, width, height }]}
-          source={{uri: 'https://tylermcginnis.com/tylermcginnis_glasses-300.png'}}
-        />
-      </View>
-    )
-  }
-}
