de186234b296112c2fd17ea79bdc687bb0c2e165
David Mrnustik <david.mrnustik@hotmail.com>
2018-02-25 17:40:05 +0100

    Animated views

--- App.js
+++ App.js
@@ -4,108 +4,46 @@ import { StyleSheet, Text, View, AppRegistry, ScrollView, Image, TouchableOpacit
 import Header from './Header';
 import { TabNavigator, StackNavigator, DrawerNavigator } from 'react-navigation';
 
-function CustomHeader ({ navigation }) {
-  <View style={{height: 80, marginTop: 20}}>
-    <View>
-      <Text>Back</Text>
-    </View>
-    <View>
-      <Title>Title</Title>
-    </View>
-    <View>
-      <Text>Right</Text>
-    </View>
-  </View>
-}
-function Home ({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <Text>HOME VIEW</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('DrawerOpen')}>
-        <Text>Open drawer</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate('Dashboard')}>
-        <Text>Go to dashboard</Text>
-      </TouchableOpacity>
-    </View>
-  )
-}
-function Dashboard( { navigation }) {
-  return (
-    <View style={styles.container}>
-      <Text>Dashboard</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('DrawerOpen')}>
-        <Text>Open drawer from Dashboard</Text>
-      </TouchableOpacity>
-      <Text>Navigation state params: {navigation.state.params}</Text>
-    </View>
-  )
-}
-const Drawer = DrawerNavigator({
-  Home: {
-    screen: Home,
-    navigationOptions: {
-      title: 'Home',
-      drawerLabel: 'Home',
-      drawerIcon: () => <FontAwesome name='home' size={20} color='red' />
-    }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-  Dashboard: {
-    screen: Dashboard,
-    navigationOptions: {
-      title: 'Dashboard',
-      drawerLabel: 'Dashboard',
-      drawerIcon: () => <FontAwesome name='dashboard' size={20} color='red' />
-    },
+  img: {
+    width: 200,
+    height: 200
   }
 })
-const Stack = StackNavigator({
-  Home: {
-    screen: Drawer,
-    title: 'Home',
-    navigationOptions: {
-      header: (props) => <Header {...props} title='Home' />,
-    }
-  },
-  Dashboard: {
-    screen: Dashboard,
-    title: 'Dashboard',
-    navigationOptions: {
-      header: (props) => <Header {...props} title='Dashboard' />,
-      title: 'Dashboard',
-      headerTintColor: 'red',
-      headerStyle: {
-        backgroundColor: 'green'
-      }
-    }
-  },
-})
+
+// decay - initial velocity and slow to a stop
+// spring - spring physics model
+// timing - animated a value over time
 
 export default class App extends React.Component {
+  state = {
+    opacity: new Animated.Value(0),
+    width: new Animated.Value(0),
+    height: new Animated.Value(0)
+  }
+  componentDidMount() {
+    const { opacity, width, height } = this.state;
+
+    Animated.timing(opacity, { toValue: 1, duration: 1000 })
+      .start()
+
+    Animated.spring(width, { toValue: 300, speed: 5}).start()
+    Animated.spring(height, { toValue: 300, speed: 5}).start()
+  }
   render() {
+    const { opacity, width, height } = this.state;
     return (
       <View style={styles.container}>
-        <Stack />
+        <Animated.Image
+          style={[styles.img, { opacity, width, height }]}
+          source={{uri: 'https://tylermcginnis.com/tylermcginnis_glasses-300.png'}}
+        />
       </View>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 20,
-    // backgroundColor: 'yellow',
-  },
-  box: {
-    // height: 50,
-    // width: 50,
-    backgroundColor: '#e76e63',
-    // margin: 10,
-  },
-  innerContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-});
